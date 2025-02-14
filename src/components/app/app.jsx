7c8bfa8a8c3aef48a2258996e4400df05a763f4e
Vasily Zotov <vazotov@t1-consulting.ru>
2025-02-14 23:42:27 +0300

    Add register page
    Move app content to Main page.
    Add routes to app

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,55 +1,19 @@
-import AppHeader from "../app-header/app-header";
-import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import curStyle from "./app.module.css";
-import BurgerConstructor from "../burger-constructor/burger-constructor";
-import React from "react";
-import Modal from "../modal/modal";
-import IngredientDetails from "../ingredient-details/ingredient-details";
-import OrderDetails from "../order-details/order-details";
+import Root from "../../pages/root/root";
+import Main from "../../pages/main/main";
+import Register from "../../pages/register/register";
 
-import { useDispatch, useSelector } from 'react-redux';
-import { loadIngredients } from "../../services/actions/ingredient";
-import { HIDE_INGREDIENT_DETAILS } from "../../services/actions/ingredient-details";
-import { HIDE_ORDER } from "../../services/actions/order";
-
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-    const dispatch = useDispatch();
-    const showDetail = useSelector(state => state.ingredientDetail.showDetail);
-    const showOrder = useSelector(state => state.order.showOrder);
-
-    // Получить ингредиенты
-    React.useState(() => {
-        dispatch(loadIngredients);
-    }, []);
-
     return (
-        <div className={curStyle.root_div}>
-            <AppHeader />
-            <DndProvider backend={HTML5Backend}>
-                <div className={curStyle.main_div}>
-                    <div className={curStyle.child_div}>
-                        <p className={`text text_type_main-large ${curStyle.constructor_title}`}>Соберите бургер</p>
-                        <BurgerIngredients />
-                    </div>
-                    <div className={curStyle.child_div}>
-                        <BurgerConstructor />
-                    </div>
-                </div>
-            </DndProvider>
-            {showDetail && (
-                <Modal isOpen={showDetail} title="Детали ингредиента" onClose={() => dispatch({ type: HIDE_INGREDIENT_DETAILS })}>
-                    <IngredientDetails />
-                </Modal>
-            )}
-            {showOrder && (
-                <Modal isOpen={showOrder} onClose={() => dispatch({ type: HIDE_ORDER })}>
-                    <OrderDetails />
-                </Modal>
-            )}
-        </div>
+        <Root>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" Component={Main} exact />
+                    <Route path="/register" Component={Register} exact />
+                </Routes>
+            </BrowserRouter>
+        </Root>
     );
 }
 
---
+++ src/pages/register/register.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import curStyle from "./register.module.css";
+import { Input, EmailInput, PasswordInput, Button } from '@ya.praktikum/react-developer-burger-ui-components';
+
+const Register = () => {
+    return (
+        <div className={curStyle.top_div}>
+            <div className={curStyle.child_div}>
+                <p className="text text_type_main-default">Регистрация</p>
+                <Input type={'text'}
+                    placeholder={'Имя'}
+                    name={'login'}
+                    onChange={e => console.log(e.target.value)} />
+                <EmailInput
+                    onChange={e => console.log(e.target.value)}
+                    value={''}
+                    name={'email'} />
+                <PasswordInput
+                    onChange={e => console.log(e.target.value)}
+                    value={''}
+                    name={'password'}
+                    extraClass="mb-2"
+                />
+                <Button htmlType="button" type="primary" size="medium">
+                    Зарегистрироваться
+                </Button>
+                <p className={`text text_type_main-default text_color_inactive ${curStyle.register_p}`}>
+                    Уже зарегестрированы?&nbsp;
+                    <a href="#">Войти</a>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Register;
