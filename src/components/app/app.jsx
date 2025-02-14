fe712db82e8664f81885a2135a4ee3b1648edb70
Vasily Zotov <vazotov@t1-consulting.ru>
2025-02-14 23:57:15 +0300

    Add Login page
    Fix center position fot Register page

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,7 @@
 import Root from "../../pages/root/root";
 import Main from "../../pages/main/main";
 import Register from "../../pages/register/register";
+import Login from "../../pages/login/login";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -11,6 +12,7 @@ function App() {
                 <Routes>
                     <Route path="/" Component={Main} exact />
                     <Route path="/register" Component={Register} exact />
+                    <Route path="/login" Component={Login} exact />
                 </Routes>
             </BrowserRouter>
         </Root>
