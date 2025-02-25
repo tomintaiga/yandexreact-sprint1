87de99e3292953270698ed01282a7f0231b58bdb
Vasily Zotov <vazotov@t1-consulting.ru>
2025-02-25 23:08:33 +0300

    Finish ingredient modal

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,37 +8,63 @@ import Profile from "../../pages/profile/profile";
 import Ingredient from "../../pages/ingredient/ingredient";
 import NotFound from "../../pages/not-found/not-found";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { loadIngredients } from "../../services/actions/ingredient";
 import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import ProtectedRouteElement from "../protected-route-element/protected-route-element";
+import Modal from "../modal/modal";
 
 function App() {
     const dispatch = useDispatch();
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const background = location.state && location.state.background;
+    const ingredientId = location.state && location.state.id;
+    const ingredient = useSelector((state) => {
+        if(!ingredientId) {
+            return null;
+        }
+        return state.ingredient.ingredients.find((item) => item._id === ingredientId);
+    });
 
     useState(() => {
         dispatch(loadIngredients);
     }, []);
 
+    const handleClose = () => {
+        console.log("close");
+        navigate(-1);
+    }
+
     return (
-        <BrowserRouter>
-            <Root>
+        <Root>
+            <Routes location={background || location}>
+                <Route path="/register" Component={Register} exact />
+                <Route path="/login" Component={Login} exact />
+                <Route path="/forgot-password" Component={ForgotPassword} exact />
+                <Route path="/reset-password" Component={ResetPassword} exact />
+
+                <Route path="/" element={<ProtectedRouteElement><Main /></ProtectedRouteElement>} exact />
+                <Route path="/profile" element={<ProtectedRouteElement><Profile /></ProtectedRouteElement>} exact />
+                <Route path="/ingredients/:id" element={<ProtectedRouteElement><Ingredient /></ProtectedRouteElement>} exact />
+
+                <Route path="*" Component={NotFound} />
+            </Routes>
+
+            {background && (
                 <Routes>
-                    <Route path="/register" Component={Register} exact />
-                    <Route path="/login" Component={Login} exact />
-                    <Route path="/forgot-password" Component={ForgotPassword} exact />
-                    <Route path="/reset-password" Component={ResetPassword} exact />
-
-                    <Route path="/" element={<ProtectedRouteElement><Main /></ProtectedRouteElement>} exact />
-                    <Route path="/profile" element={<ProtectedRouteElement><Profile/></ProtectedRouteElement>} exact />
-                    <Route path="/ingredients/:id" element={<ProtectedRouteElement><Ingredient/></ProtectedRouteElement>} exact />
-
-                    <Route path="*" Component={NotFound} />
+                    <Route path="/ingredients/:id" element={
+                        <Modal onClose={handleClose} isOpen={true} title={ingredient?.name}>
+                            <Ingredient />
+                        </Modal>
+                    } />
                 </Routes>
+            )}
 
-            </Root>
-        </BrowserRouter>
+        </Root>
     );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,8 @@ import { thunk } from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { rootReducer } from './services/reducers';
 
+import { BrowserRouter } from 'react-router-dom';
+
 const composeEnhancers =
   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
@@ -24,7 +26,9 @@ const store = createStore(rootReducer, enhancer);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store} >
-      <App/>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 )
