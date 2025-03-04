430961cf4c362c63002e1a4715ea35d7606036ec
Vasily Zotov <vazotov@t1-consulting.ru>
2025-03-04 21:59:34 +0300

    Add typescript tp project
    Rewrite AppHeader to TS

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -9,7 +9,9 @@ import { Link } from 'react-router-dom';
 
 import headerStyle from './app-header.module.css';
 
-const AppHeader = () => {
+import React from 'react';
+
+const AppHeader: React.FC = () => {
   return (
     <div className={headerStyle.header_div}>
       <div className={headerStyle.inner_flex}>
